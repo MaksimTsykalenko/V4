e0a905a98722ed4f3bec5d8d05d89104a1031b46
Maksim Tsykalenko <tsykalenkom@gmail.com>
2021-04-21 13:24:15 +0300

    Add Dockerfile

--- app/api/shows.py
+++ app/api/shows.py
@@ -7,8 +7,8 @@ from app.api.dependencies.shows import get_shows
 router = APIRouter()
 
 @router.get("/{ch_id}")
-async def get_shows(chanel_id: int,
+async def get_shows(ch_id: int,
         start_t: Optional[datetime]=None,
         end_t: Optional[datetime]=None,
         shows: ShowRepository = Depends(get_shows)):
-    return await shows.get_list(chanel_id=chanel_id,start_t=start_t, end_t=end_t)
+    return await shows.get_list(chanel_id=ch_id,start_t=start_t, end_t=end_t)
